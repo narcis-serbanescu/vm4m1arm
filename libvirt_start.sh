5707ca8729209d0c87c9ed66af78cabfbf01a10a
Narcis Serbanescu <narcis_serbanescu@ro.ibm.com>
2023-01-30 12:50:06 +0200

    Update libvirt_start.sh
    Added "accelerate" option so that MAC OS chooses the right vCPU.

--- libvirt_start.sh
+++ libvirt_start.sh
@@ -1,16 +1,24 @@
 #!/bin/zsh
+VMNAME=fs37libvirt2
+ISOPATH=Fedora-Server-dvd-aarch64-37-1.7.iso
+PASSWD='sudo_root_password'
 
 virt-install --connect qemu:///session \
+	--name ${VMNAME} \
+	--description "This is ${VMNAME}" \
 	-v \
 	--virt-type qemu \
-       	-n fs37libvirt \
+       	-n ${VMNAME} \
        	-r 4098 \
 	--vcpus=2 \
-	--disk path=fs36libvirt.qcow2,device=disk,bus=virtio,sparse=true,size=10,cache=none \
+	--accelerate \
+	--disk path=${VMNAME}.qcow2,device=disk,bus=virtio,sparse=true,size=10,cache=none \
 	--cdrom=Fedora-Server-dvd-aarch64-37-1.7.iso \
 	--graphics=vnc \
 	--network=user \
 	--os-variant=fedora37 \
-	--import \
 	--arch aarch64 \
 	--force
+
+echo "VNC connection: "
+virsh vncdisplay ${VMNAME}
